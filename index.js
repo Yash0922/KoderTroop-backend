95fcee5dc8da07c7adfe74b1d3e05bb9eb3ca88d
yash0922 <yashkumarpal987@gmail.com>
2023-06-14 00:00:30 +0530

    update full backend

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
 const { Client } = require('@elastic/elasticsearch');
-const redis = require('redis');
+// const redis = require('redis');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const https = require('https');
@@ -10,11 +10,30 @@ const https = require('https');
 
 const app = express();
 
-const esClient = new Client({ node: 'http://localhost:9200'
+const esClient = new Client({ node: 'https://localhost:9200',
+auth: {
+  username: 'elastic',
+  password: 'GoiKGG8r+e+XeUY*4v3I',
+},
+tls:{
+  ca: fs.readFileSync('./http_ca.crt', 'utf8'),
+        rejectUnauthorized: true
+}
 },
 );
 
-const redisClient = redis.createClient();
+// const redisClient = redis.createClient({ host: '127.0.0.1',
+// port: 6379,
+// });
+
+
+// redisClient.on('connect', () => {
+//   console.log('Redis client connected');
+// });
+
+// redisClient.on('error', (error) => {
+//   console.error('Redis error:', error);
+// });
 
 // Connect to MongoDB
 mongoose.connect('mongodb+srv://yashkumarpal987:Yash%4015182204@cluster0.ss532hp.mongodb.net/todo', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -24,8 +43,9 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // Define task schema and model
 const taskSchema = new mongoose.Schema({
   _id: String,
-  title: String,
-  description: String
+  name: String,
+  complete:Boolean,
+
 });
 const Task = mongoose.model('Task', taskSchema);
 
@@ -35,30 +55,42 @@ app.use(express.json());
 // Task creation
 app.post('/tasks', async (req, res) => {
   try {
-    const { title, description } = req.body;
+    const { name} = req.body;
     const taskId = uuidv4();
 
     // Store task in MongoDB
-    const task = new Task({ _id: taskId, title, description });
+    const task = new Task({ _id: taskId, name, complete:false  });
     await task.save();
 
     // Store task in ElasticSearch
-    await esClient.indices.create({
-      index: 'tasks',
-      body: {
-        mappings: {
-          properties: {
-            title: { type: 'text' },
-            description: { type: 'text' }
-          }
-        }
-      }
-    });
-
+    // await esClient.indices.create({
+    //   index: 'tasks',
+    //   body: {
+    //     mappings: {
+    //       properties: {
+    //         name: { type: 'text' },
+            
+    //       }
+    //     }
+    //   }
+    // });
+    // let result  = await esClient.index({
+    //   index: 'tasks',
+    //   body: {
+    //     name
+    //   }
+    // })
+// console.log(result);
     // Store task in Redis
-    redisClient.hmset(taskId, { title, description });
+    // redisClient.hSet(taskId, 'name', name, (error, reply) => {
+    //   if (error) {
+    //     console.error('Error storing task in Redis:', error);
+    //     return;
+    //   }
+    //   console.log('Task stored in Redis:', reply);
+    // });
 
-    res.json({ message: 'Task created successfully' });
+    res.json({ message: 'Task created successfully',result: task});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
@@ -71,6 +103,7 @@ app.get('/tasks', async (req, res) => {
     const tasks = await Task.find({});
 
     res.json({ tasks });
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
@@ -81,24 +114,48 @@ app.get('/tasks', async (req, res) => {
 app.get('/tasks/search', async (req, res) => {
   try {
     const { query } = req.query;
-    await Task.find({ title: query });
+    console.log(query);
+   let result = await Task.find({ name: query });
 
     // Search tasks in ElasticSearch
-    const { body } = await esClient.search({
-      index: 'tasks',
-      body: {
-        query: {
-          match: { title: query }
-        }
-      }
-    });
+    // const result = await esClient.search({
+    //   index: 'tasks',
+    //   body: {
+    //     query: {
+    //       match: { name: query }
+    //     }
+    //   }
+    // });
      
 
     // res.json({ tasksMongo });
+console.log(result)
 
-    const tasks = body.hits.hits.map(hit => hit._source);
+    // const tasks = body.hits.hits.map(hit => hit._source);
 
-    res.json({ tasks });
+    // res.json({ result: result.hits.hits});
+    res.json({ result: result});
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'An error occurred' });
+  }
+});
+app.patch('/tasks/:taskId', async (req, res) => {
+  try {
+    const { taskId } = req.params;
+
+    // Find the task in MongoDB
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found' });
+    }
+
+    // Toggle the completion status
+    task.complete = !task.complete;
+    await task.save();
+
+    res.json({ message: 'Task completion status toggled successfully', result: task });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
@@ -111,18 +168,15 @@ app.delete('/tasks/:taskId', async (req, res) => {
     const { taskId } = req.params;
 
     // Delete task from MongoDB
-    await Task.deleteOne({ _id: taskId });
+    let result  = await Task.deleteOne({ _id: taskId });
 
     // Delete task from ElasticSearch
-    await esClient.delete({
-      index: 'tasks',
-      id: taskId
-    });
+    
 
     // Delete task from Redis
-    await redisClient.delAsync(taskId);
+    // await redisClient.delAsync(taskId);
 
-    res.json({ message: 'Task deleted successfully' });
+    res.json({ message: 'Task deleted successfully',result:result });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
@@ -130,6 +184,6 @@ app.delete('/tasks/:taskId', async (req, res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('http://localhost:3000');
+app.listen(3001, () => {
+  console.log('http://localhost:3001');
 });
